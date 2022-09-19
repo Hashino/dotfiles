2ed5472c58c19605250d4209a369d75989c25fcd
Hashino <iamravara@gmail.com>
2022-09-19 02:25:07 -0300

    re added exa-zsh
    trying to figure out why it doesnt work

---
+++ .oh-my-zsh/custom/plugins/exa-zsh/exa-zsh.plugin.zsh
@@ -0,0 +1,20 @@
+# Project:  exa-zsh
+# File:     exa-zsh-plugin.zsh
+# Author:   Mohamed Elashri
+# Email:    muhammadelashri@gmail.com
+
+
+if ! (( $+commands[exa] )); then
+  print "zsh-exa-plugin: exa not found on path. Please install exa before using this plugin." >&2
+  return 1
+fi
+
+# general use aliases 
+alias ls='exa -a --icons --group-directories-first' # just replace ls by exa and allow all other exa arguments
+alias l='ls -lbF' #   list, size, type
+alias ll='ls -la' # long, all
+alias llm='ll --sort=modified' # list, long, sort by modification date
+alias la='ls -lbhHigUmuSa' # all list
+alias lx='ls -lbhHigUmuSa@' # all list and extended
+alias tree='ls --tree --icons' # tree view
+alias lS='ls -1 --icons --group-directories-first' # one column by just names
