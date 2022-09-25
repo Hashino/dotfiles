371c92d30f75de32b9a18106c1eea400b37fdf42
Hashino <iam.ravara@gmail.com>
2022-09-25 16:48:24 -0300

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -84,7 +84,8 @@ spacer =
 }
 --------------------------------------------------------------------------------------------------
 -- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
+awful.layout.layouts = 
+{
 	awful.layout.suit.tile,
 	--awful.layout.suit.tile.left,
 	--awful.layout.suit.tile.top,
