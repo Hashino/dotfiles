7a3c88fe9ea69c2edeaf245fbc3063b2a87dff1c
Hashino <iam.ravara@gmail.com>
2022-09-24 21:36:32 -0300

    tweaks

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+picom -b
+
 ## run (only once) processes which spawn with the same name
 #function run {
 #   if (command -v $1 && ! pgrep $1); then
@@ -16,5 +18,16 @@ function run {
 }
 
 
+run thunar --daemon
+run picom -b
+run pasystray --notify=all
+run /usr/lib/xfce-polkit/xfce-polkit
+
+run sudo openrgb --startminimized
+ckb-next --background
+
+
+
 #enviroment variables
 export QT_QPA_PLATFORMTHEME=gtk2
+export PF_INFO="ascii title os kernel wm shell memory palette"
