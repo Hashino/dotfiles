2a31b4149f749d4273061ddb79fa2b47ea9c3137
hashino <iam.ravara@gmail.com>
2024-01-08 09:33:39 -0300

    started scripts

--- .scripts/install_yay.sh
+++ .scripts/install_yay.sh
@@ -2,5 +2,9 @@
 # Hash's dotfiles install script
 # https://github.com/Hashino/dotfiles
 
-# install an aur helper
-sudo pacman -S --needed git base-devel --noconfirm && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel --noconfirm
+git clone https://aur.archlinux.org/yay.git "${HOME}/yay"
+cd "${HOME}/yay"
+makepkg -si --noconfirm
+
+sudo rm -R "${HOME}/yay"
