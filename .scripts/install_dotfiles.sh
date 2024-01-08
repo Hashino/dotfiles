e76d8b08a6f46e0654da93225997e250796d98b1
hashino <iam.ravara@gmail.com>
2024-01-08 17:37:31 -0300

    mostly done

--- .scripts/install_dotfiles.sh
+++ .scripts/install_dotfiles.sh
@@ -5,31 +5,47 @@
 # meant to be used right after arch install
 # TODO: make a fedora version
 
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+NORMAL="\e[0m"
+
 dotfiles_location="${HOME}/.dotfiles"
 log_file="${HOME}/.config/install.log"
 
-rm $log_file
+check_success () {
+  if [ $? -eq 0 ]; then
+    echo -e " | ${GREEN}Sucess${NORMAL}"
+  else
+    echo -e " | ${RED}Failed${NORMAL} check ${log_file} for more info"
+  fi
+}
+
+#resets install log
+echo "" > $log_file
 
 # clone dotfiles
 echo "Downloading dotfiles"
+
 git clone https://github.com/Hashino/dotfiles $dotfiles_location &> $log_file
+check_success
 
 cd "${dotfiles_location}/.config"
 
+#creates symlinks to configs in dotfiles inside ~/.config
 for app in */ ; do
   remote_config="${dotfiles_location}/.config/${app}"
   local_config="${HOME}/.config/"
-  ln -s $remote_config $local_config
-  echo "created symlink in ${local_config} to ${remote_config}"
+  
+  echo "creating symlink in ${local_config} to ${remote_config}"
+  
+  ln -s $remote_config $local_config &> $log_file
+  check_success  
 done
 
-#apps=""
-
+#install all packages listed in /.dotfiles/.scripts/pkg.list
 while read app; do
-  echo "Installing ${app}"
+  echo -n "Installing ${app}"
   yes | yay -S $app --noconfirm --askyesremovemake &> $log_file
+  check_success
 done <"${dotfiles_location}/.scripts/pkg.list"
-
-#yay -S $apps --noconfirm
-
-#yay -Syu --needed --noconfirm - < "${dotfiles_location}/.scripts/pkg.list"
