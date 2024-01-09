88ae6e26ee15a32be9272775b8f48ffa9cf68ca6
hashino <iam.ravara@gmail.com>
2024-01-09 20:00:36 -0300

    fixed spinner

--- .scripts/install_dotfiles.sh
+++ .scripts/install_dotfiles.sh
@@ -43,14 +43,15 @@ check_success () {
 }
 
 spinner() {
-  local pid=$!
-  local delay=0.75
-  local spinstr='|/-\'
-  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    local temp=${spinstr#?}
-    printf "  %c  " "$spinstr"
-    local spinstr=$temp${spinstr%"$temp"}
-    sleep $delay
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
     printf "\b\b\b\b\b\b"
   done
   printf "    \b\b\b\b"
