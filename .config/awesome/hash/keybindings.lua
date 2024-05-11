1cd0a3a0b0f50f42782c0b99a936bc2f5c5d9dfb
Hashino <iam.ravara@gmail.com>
2024-05-11 16:05:03 -0300

    changed keybindings

--- .config/awesome/hash/keybindings.lua
+++ .config/awesome/hash/keybindings.lua
@@ -1,56 +1,76 @@
 -- by Hashino https://github.com/Hashino/dotfiles
 -------------------------------------------------------------------------------------------------------------------
-local awful 		    = require("awful")
+local awful = require("awful")
 ------------------------------------------------------------------------------------------------------------------
-awful.keyboard.append_global_keybindings
-({
-  -- monitor control
-  ------------------------------------------------------------------------------------------------------------------
-	awful.key({ Global.Keys.ModKey, "Control" },            "w",     function ()
-		awful.spawn( "input_switch_237e" )
-	end, {description = "open nvim", group = "custom actions"}),
-	awful.key({ Global.Keys.ModKey,           },            "KP_Subtract",     function ()
-		awful.spawn( "profile_switch_237e" )
-	end, {description = "open nvim", group = "custom actions"}),
-  -- custom actions
-  ------------------------------------------------------------------------------------------------------------------
-	awful.key({ Global.Keys.ModKey            },            "v",     function ()
-		awful.spawn( Global.Apps.Terminal .. " -e \"nvim -c 'NvimTreeFocus'\"")
-	end, {description = "open nvim", group = "custom actions"}),
+awful.keyboard.append_global_keybindings({
+	-- monitor control
+	------------------------------------------------------------------------------------------------------------------
+	awful.key({ Global.Keys.ModKey, "Control" }, "w", function()
+		awful.spawn("input_switch_237e")
+	end, { description = "open nvim", group = "custom actions" }),
+	awful.key({ Global.Keys.ModKey }, "KP_Subtract", function()
+		awful.spawn("profile_switch_237e")
+	end, { description = "open nvim", group = "custom actions" }),
+	-- custom actions
+	------------------------------------------------------------------------------------------------------------------
+	awful.key({ Global.Keys.ModKey }, "v", function()
+		awful.spawn(Global.Apps.Terminal .. ' -e "nvim"')
+	end, { description = "open nvim", group = "custom actions" }),
 
-	awful.key({ Global.Keys.ModKey            },            "w",     function ()
-		awful.spawn( "win")
-	end, {description = "turn on windows vm (if it wasn't) and shows it", group = "custom actions"}),
-  ------------------------------------------------------------------------------------------------------------------
-  -- awesome controls
-	awful.key({ Global.Keys.ModKey,           }, "s", require("awful.hotkeys_popup").widget.show_help,
-		{description = "show help", group = "awesome"}),
-	awful.key({ Global.Keys.ModKey, "Control" }, "r", awesome.restart,
-		{description = "reload awesome", group = "awesome"}),
-------------------------------------------------------------------------------------------------------------------
-  -- client controls
-	awful.key({ Global.Keys.ModKey,           }, "Tab",     awful.tag.viewnext,
-		{description = "view next tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,           }, "Right",   awful.tag.viewnext,
-		{description = "view next tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,           }, "l",   awful.tag.viewnext,
-		{description = "view next tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,    "Shift"}, "Tab",     awful.tag.viewprev,
-		{description = "view pevious tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,           }, "Left",    awful.tag.viewprev,
-		{description = "view pevious tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,           }, "h",    awful.tag.viewprev,
-		{description = "view pevious tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,           }, "j", function () awful.client.focus.byidx(1) end,
-		{description = "focus next by index", group = "client"}),
-	awful.key({ Global.Keys.ModKey,           }, "Down", function () awful.client.focus.byidx(1) end,
-		{description = "focus next by index", group = "client"}),
-	awful.key({ Global.Keys.ModKey,           }, "k", function () awful.client.focus.byidx(-1) end,
-		{description = "focus previous by index", group = "client"}),
-	awful.key({ Global.Keys.ModKey,           }, "Up", function () awful.client.focus.byidx(-1) end,
-		{description = "focus previous by index", group = "client"}),
-------------------------------------------------------------------------------------------------------------------
---[[
+	awful.key({ Global.Keys.ModKey }, "w", function()
+		awful.spawn("win")
+	end, { description = "turn on windows vm (if it wasn't) and shows it", group = "custom actions" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- awesome controls
+	awful.key(
+		{ Global.Keys.ModKey },
+		"s",
+		require("awful.hotkeys_popup").widget.show_help,
+		{ description = "show help", group = "awesome" }
+	),
+	awful.key(
+		{ Global.Keys.ModKey, "Control" },
+		"r",
+		awesome.restart,
+		{ description = "reload awesome", group = "awesome" }
+	),
+	------------------------------------------------------------------------------------------------------------------
+	-- client controls
+	awful.key({ Global.Keys.ModKey }, "Tab", awful.tag.viewnext, { description = "view next tag", group = "tag-nav" }),
+	awful.key(
+		{ Global.Keys.ModKey },
+		"Right",
+		awful.tag.viewnext,
+		{ description = "view next tag", group = "tag-nav" }
+	),
+	awful.key({ Global.Keys.ModKey }, "l", awful.tag.viewnext, { description = "view next tag", group = "tag-nav" }),
+	awful.key(
+		{ Global.Keys.ModKey, "Shift" },
+		"Tab",
+		awful.tag.viewprev,
+		{ description = "view pevious tag", group = "tag-nav" }
+	),
+	awful.key(
+		{ Global.Keys.ModKey },
+		"Left",
+		awful.tag.viewprev,
+		{ description = "view pevious tag", group = "tag-nav" }
+	),
+	awful.key({ Global.Keys.ModKey }, "h", awful.tag.viewprev, { description = "view pevious tag", group = "tag-nav" }),
+	awful.key({ Global.Keys.ModKey }, "j", function()
+		awful.client.focus.byidx(1)
+	end, { description = "focus next by index", group = "client" }),
+	awful.key({ Global.Keys.ModKey }, "Down", function()
+		awful.client.focus.byidx(1)
+	end, { description = "focus next by index", group = "client" }),
+	awful.key({ Global.Keys.ModKey }, "k", function()
+		awful.client.focus.byidx(-1)
+	end, { description = "focus previous by index", group = "client" }),
+	awful.key({ Global.Keys.ModKey }, "Up", function()
+		awful.client.focus.byidx(-1)
+	end, { description = "focus previous by index", group = "client" }),
+	------------------------------------------------------------------------------------------------------------------
+	--[[
   awful.key({ Global.Keys.ModKey,           }, "d", function ()
 		if #awful.screen.focused().clients > 0 then
 			for _, c in ipairs(mouse.screen.selected_tag:clients()) do
@@ -66,123 +86,124 @@ awful.keyboard.append_global_keybindings
 	end,
 	{description = "(un)minimize all clients", group = "client"}),
   ]]
-  --------------------------------------------------------------------------------------------------------------------
-  -- tag controls
-	awful.key({ Global.Keys.ModKey,  "Control"}, "n",
-		function ()
-			if #root.tags() < 9 then
-				awful.tag.add("", {
+	--------------------------------------------------------------------------------------------------------------------
+	-- tag controls
+	awful.key({ Global.Keys.ModKey, "Control" }, "n", function()
+		if #root.tags() < 9 then
+			awful.tag
+				.add("", {
 					screen = awful.screen.focused(),
-					layout = awful.layout.layouts[1] }):view_only()
-			end
-		end,
-		{description = "add tag", group = "tag-nav"}),
-	awful.key({ Global.Keys.ModKey,  "Control"}, "x",
-		function ()
-			if #root.tags() > 1 then
-				awful.screen.focused().selected_tag:delete()
-			end
-		end,
-		{description = "remove tag", group = "tag-nav"}),
-------------------------------------------------------------------------------------------------------------------
-  -- Standard programs
-	awful.key({ Global.Keys.ModKey,           }, "Return", function ()
+					layout = awful.layout.layouts[1],
+				})
+				:view_only()
+		end
+	end, { description = "add tag", group = "tag-nav" }),
+	awful.key({ Global.Keys.ModKey, "Control" }, "x", function()
+		if #root.tags() > 1 then
+			awful.screen.focused().selected_tag:delete()
+		end
+	end, { description = "remove tag", group = "tag-nav" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- Standard programs
+	awful.key({ Global.Keys.ModKey }, "Return", function()
 		awful.spawn(Global.Apps.Terminal)
-	end, {description = "open a terminal", group = "launcher"}),
-	awful.key({ Global.Keys.ModKey,           }, "b", function ()
+	end, { description = "open a terminal", group = "launcher" }),
+	awful.key({ Global.Keys.ModKey }, "b", function()
 		awful.spawn(Global.Apps.Browser)
-	end, {description = "launch Browser", group = "launcher"}),
-	awful.key({ Global.Keys.ModKey,           }, "e", function ()
+	end, { description = "launch Browser", group = "launcher" }),
+	awful.key({ Global.Keys.ModKey }, "e", function()
 		awful.spawn(Global.Apps.Filemanager)
-	end, {description = "launch filemanager", group = "launcher"}),
-------------------------------------------------------------------------------------------------------------------
-  -- rofi
-	awful.key({ Global.Keys.ModKey },            "r",     function ()
+	end, { description = "launch filemanager", group = "launcher" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- rofi
+	awful.key({ Global.Keys.ModKey }, "r", function()
 		awful.spawn("rofi -show run")
-	end, {description = "rofi run", group = "rofi"}),
-	awful.key({ Global.Keys.ModKey },            "Escape",     function ()
+	end, { description = "rofi run", group = "rofi" }),
+	awful.key({ Global.Keys.ModKey }, "Escape", function()
 		awful.spawn("rofi -show power-menu -modi power-menu:~/.local/bin/rofi-power-menu")
-	end, {description = "rofi powermenu", group = "rofi"}),
-	awful.key({ Global.Keys.ModKey },            "t",     function ()
+	end, { description = "rofi powermenu", group = "rofi" }),
+	awful.key({ Global.Keys.ModKey }, "t", function()
 		awful.spawn("rofi-todo -f todo")
-	end, {description = "rofi todo", group = "rofi"}),
-------------------------------------------------------------------------------------------------------------------
-  -- screenshot
-	awful.key({ Global.Keys.ModKey, "Control" }, "p",     function ()
+	end, { description = "rofi todo", group = "rofi" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- screenshot
+	awful.key({ Global.Keys.ModKey, "Control" }, "p", function()
 		awful.spawn("scrot -s -o -f print.png -e 'xclip -selection clipboard -t image/png -i $f'")
-	end, {description = "screenshot selection", group = "screenshot"}),
-	awful.key({ Global.Keys.ModKey,           }, "p",     function ()
+	end, { description = "screenshot selection", group = "screenshot" }),
+	awful.key({ Global.Keys.ModKey }, "p", function()
 		awful.spawn("scrot -o -f print.png -e 'xclip -selection clipboard -t image/png -i $f'")
-	end, {description = "screenshot entire screen", group = "screenshot"}),
-------------------------------------------------------------------------------------------------------------------
-  -- media
-	awful.key({ }, "XF86AudioRaiseVolume",     function ()
+	end, { description = "screenshot entire screen", group = "screenshot" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- media
+	awful.key({}, "XF86AudioRaiseVolume", function()
 		awful.spawn("pactl -- set-sink-volume 0 +3%")
-	end, {description = "volume +/-", group = "media"}),
-	awful.key({ }, "XF86AudioLowerVolume",     function ()
+	end, { description = "volume +/-", group = "media" }),
+	awful.key({}, "XF86AudioLowerVolume", function()
 		awful.spawn("pactl -- set-sink-volume 0 -3%")
-	end, {description = "volume +/-", group = "media"}),
-	awful.key({ }, "XF86AudioMute",     function ()
+	end, { description = "volume +/-", group = "media" }),
+	awful.key({}, "XF86AudioMute", function()
 		awful.spawn("pactl set-sink-mute 0 toggle")
-	end, {description = "(un)mute", group = "media"}),
-	awful.key({ }, "XF86AudioPlay",     function ()
+	end, { description = "(un)mute", group = "media" }),
+	awful.key({}, "XF86AudioPlay", function()
 		awful.spawn("playerctl -a play-pause")
-	end, {description = "play/pause", group = "media"}),
-	awful.key({ }, "XF86AudioPrev",     function ()
+	end, { description = "play/pause", group = "media" }),
+	awful.key({}, "XF86AudioPrev", function()
 		awful.spawn("playerctl previous")
-	end, {description = "previous", group = "media"}),
-	awful.key({ }, "XF86AudioNext",     function ()
+	end, { description = "previous", group = "media" }),
+	awful.key({}, "XF86AudioNext", function()
 		awful.spawn("playerctl next")
-	end, {description = "next", group = "media"}),
-------------------------------------------------------------------------------------------------------------------
-  -- control
-	awful.key({ }, "XF86MonBrightnessUp",     function ()
+	end, { description = "next", group = "media" }),
+	------------------------------------------------------------------------------------------------------------------
+	-- control
+	awful.key({}, "XF86MonBrightnessUp", function()
 		awful.spawn("brightnessctl s +5%")
-	end, {description = "brightness +/-", group = "control"}),
-	awful.key({ }, "XF86MonBrightnessDown",     function ()
+	end, { description = "brightness +/-", group = "control" }),
+	awful.key({}, "XF86MonBrightnessDown", function()
 		awful.spawn("brightnessctl s 5%-")
-	end, {description = "brightness +/-", group = "control"}),
+	end, { description = "brightness +/-", group = "control" }),
 })
 ------------------------------------------------------------------------------------------------------------------
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do awful.keyboard.append_global_keybindings ({
+for i = 1, 9 do
+	awful.keyboard.append_global_keybindings({
 		-- View tag only.
-		awful.key({ Global.Keys.ModKey }, "#" .. i + 9,
-			function ()
-				local screen = awful.screen.focused()
-				local tag = screen.tags[i]
-				if tag then
-					tag:view_only()
-				end
-			end),
+		awful.key({ Global.Keys.ModKey }, "#" .. i + 9, function()
+			local screen = awful.screen.focused()
+			local tag = screen.tags[i]
+			if tag then
+				tag:view_only()
+			end
+		end),
 		-- Move client to tag.
-		awful.key({ Global.Keys.ModKey, "Shift" }, "#" .. i + 9,
-			function ()
-				if client.focus then
-					local tag = client.focus.screen.tags[i]
-					if tag then
-						client.focus:move_to_tag(tag)
-					end
+		awful.key({ Global.Keys.ModKey, "Shift" }, "#" .. i + 9, function()
+			if client.focus then
+				local tag = client.focus.screen.tags[i]
+				if tag then
+					client.focus:move_to_tag(tag)
 				end
-			end)
+			end
+		end),
 	})
 end
 ------------------------------------------------------------------------------------------------------------------
 client.connect_signal("request::default_mousebindings", function()
-    awful.mouse.append_client_mousebindings({
-		awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+	awful.mouse.append_client_mousebindings({
+		awful.button({}, 1, function(c)
+			client.focus = c
+			c:raise()
+		end),
 		awful.button({ Global.Keys.ModKey }, 1, awful.mouse.client.move),
-		awful.button({ Global.Keys.ModKey }, 3, awful.mouse.client.resize)
-    })
+		awful.button({ Global.Keys.ModKey }, 3, awful.mouse.client.resize),
+	})
 end)
 -----------------------------------------------------------------------------------------------------------------------
 client.connect_signal("request::default_keybindings", function()
-	awful.keyboard.append_client_keybindings
-	({
-		awful.key({ Global.Keys.ModKey }, "x", function (c) c:kill() end,
-        {description = "close", group = "client"}),
+	awful.keyboard.append_client_keybindings({
+		awful.key({ Global.Keys.ModKey }, "x", function(c)
+			c:kill()
+		end, { description = "close", group = "client" }),
 		--[[
       wful.key({ Global.Keys.ModKey }, "n",
 			function (c)
@@ -192,21 +213,21 @@ client.connect_signal("request::default_keybindings", function()
 			end ,
 			{description = "minimize", group = "client"}),
       ]]
-		awful.key({ Global.Keys.ModKey }, "m",
-			function (c)
-				c.maximized = not c.maximized
-				c:raise()
-			end ,
-		{description = "(un)maximize", group = "client"}),
+		awful.key({ Global.Keys.ModKey }, "m", function(c)
+			c.maximized = not c.maximized
+			c:raise()
+		end, { description = "(un)maximize", group = "client" }),
 
-		awful.key({ Global.Keys.ModKey }, "f", awful.client.floating.toggle,
-		{description = "toggle floating", group = "client"}),
-		awful.key({ Global.Keys.ModKey, "Control" }, "f",
-		function (c)
+		awful.key(
+			{ Global.Keys.ModKey },
+			"f",
+			awful.client.floating.toggle,
+			{ description = "toggle floating", group = "client" }
+		),
+		awful.key({ Global.Keys.ModKey, "Control" }, "f", function(c)
 			c.fullscreen = not c.fullscreen
 			c:raise()
-		end,
-		{description = "toggle fullscreen", group = "client"}),
+		end, { description = "toggle fullscreen", group = "client" }),
 	})
 end)
 ------------------------------------------------------------------------------------------------------------------
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,17 +26,17 @@ alias tree 'ls --tree --icons' # tree view
 alias lS 'ls -1 --icons --group-directories-first' # one column by just names
 
 ##vim
-alias v "nvim -c 'NvimTreeToggle' ." 
-alias vi "nvim -c 'NvimTreeToggle' ." 
-alias vim "nvim -c 'NvimTreeToggle' ." 
-alias nvim "nvim -c 'NvimTreeToggle' ." 
-alias neovim "nvim -c 'NvimTreeToggle' ." 
+alias v "nvim" # -c 'NvimTreeToggle'." 
+alias vi "nvim" # -c 'NvimTreeToggle' ." 
+alias vim "nvim" # -c 'NvimTreeToggle' ." 
+alias nvim "nvim" # -c 'NvimTreeToggle' ." 
+alias neovim "nvim" # -c 'NvimTreeToggle' ." 
 
 #yay-list
 alias yay-list "comm -23 <(yay -Qqe | sort) <({ yay -Qqg base-devel; expac -l '\n' '%E' base; } | sort -u) | fzf --preview 'yay -Qil {}' --layout=reverse --bind 'enter:execute(yay -Qil {} | more)'"
 
 #package manager
-alias pm "yay -Qqent | fzf --preview 'yay -Qil {}' --layout=reverse --bind 'enter:execute(yay -Qil {} | less)' --bind 'ctrl-u:execute(yay -Rns {})' --bind 'ctrl-o:execute(yay -Qtdq | yay -Rns - )' --header='[ENTER] = Details    [CTRL+U] = Uninstall    [CTRL+O] = Remove Orphans' "
+alias pm "yay -Qqen | fzf --preview 'yay -Qil {}' --layout=reverse --bind 'enter:execute(yay -Qil {} | less)' --bind 'ctrl-u:execute(yay -Rns {})' --bind 'ctrl-o:execute(yay -Qtdq | yay -Rns - )' --header='[ENTER] = Details    [CTRL+U] = Uninstall    [CTRL+O] = Remove Orphans' "
 
 #remove orphan packages
 #alias orphans "yay -Qtdq | yay -Rns - || echo 'no orphan packages found'"
@@ -48,7 +48,7 @@ set -U -x PAGER moar
 
 set -U -x PF_INFO "ascii title os kernel wm shell memory palette"
 
-# i hate qt applications
+# I hate qt applications
 set -U -x QT_QPA_PLATFORMTHEME qt5ct:qt6ct
 #set -U -x CALIBRE_USE_DARK_PALETTE 1
 set -U -x CALIBRE_USE_SYSTEM_THEME 1
