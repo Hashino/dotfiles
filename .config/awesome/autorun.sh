04dda8e08e70e040bfe9ae8a93ba136069146f34
Hashino <iam.ravara@gmail.com>
2024-08-27 11:25:33 -0300

    autoruns notebook applets
    auto detects if running on a notebook and runs relevant applets if so
    
    fixes #3

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 function run {
-  if ! pgrep -f $1 ;
-  then
-    $@& disown > "${HOME}/.local/share/xorg/startup/${1}.log" 2>&1
+  if ! pgrep -f $1; then
+    $@ &
+    disown >"${HOME}/.local/share/xorg/startup/${1}.log" 2>&1
   fi
 }
 
+computer_type=$(cat /sys/class/dmi/id/chassis_type)
+
+# detects if running on a notebook
+if [ "$computer_type" == "8" ] || [ "$computer_type" == "9" ] || [ "$computer_type" == "10" ]; then
+  # network applet
+  run nm-applet
+  # battery indicator
+  run cbatticon
+fi
+
+# compositor
 run picom -b
 
+# audio applet
 run pasystray --notify=all
-
-#run nm-applet
-
-# polkit
-run /usr/lib/xfce-polkit/xfce-polkit
-
-# keyring
-run dbus-update-activation-environment --all
-run gnome-keyring-daemon --start --components=secrets
 
 # lock screen
 run xset s 1800 5
@@ -26,6 +29,13 @@ run xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock
 # makes mouse disappear
 run unclutter -idle 3
 
+# polkit
+run /usr/lib/xfce-polkit/xfce-polkit
+
+# keyring
+run dbus-update-activation-environment --all
+run gnome-keyring-daemon --start --components=secrets
+
 # rgb stuff
 run openrgb -p default_off.orp
-ckb-next --profile default  --background --close
+ckb-next --profile default --background --close
