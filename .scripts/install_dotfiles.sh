f7a582fc32037ed77a292ea1a5467ae0a053c92a
Hashino <iam.ravara@gmail.com>
2024-12-19 12:52:11 -0300

    fixing install script

--- .scripts/install_dotfiles.sh
+++ .scripts/install_dotfiles.sh
@@ -14,7 +14,7 @@ dotfiles_remote="https://github.com/Hashino/dotfiles"
 dotfiles_local="${HOME}/.dotfiles"
 log_file="${HOME}/install.log"
 
-####################################################################################################
+##############################################################################
 # GLOBAL VARIABLES
 
 user=${USER}
@@ -25,7 +25,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 BLUE='\033[1;34m'
 ORANGE='\033[1;33m'
-ORANGE_NORMAL='\033[1;33m'
+ORANGE_NORMAL='\033[0;33m'
 NORMAL='\033[0;37m'
 BOLD='\033[1;37m'
 
@@ -34,8 +34,18 @@ QUOTE='\033[4;36m'
 
 RES_COL=100
 
-####################################################################################################
+##############################################################################
 # FUNCTIONS
+
+echof() {
+  if [ $# -eq 2 ]; then
+    echo -e $1$2$NORMAL
+  elif [ $# -eq 3 ]; then
+    echo -e -n $1$2$NORMAL
+  fi
+  echo -e $2 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" >> $log_file
+  return $?
+}
 
 check_success () {
   if [ $? -eq 0 ]; then
@@ -68,7 +78,7 @@ check_success () {
     return 1
   fi
   rc=$?
-  echo
+  printf "\n\r"
   return $rc
 }
 
@@ -95,7 +105,7 @@ print_center(){
   printf "%*s\n" $(( (${#text} + columns) / 2)) "$text"
 }
 
-####################################################################################################
+##############################################################################
 # START
 clear
 
@@ -121,13 +131,10 @@ fi
 sleep 0.5
 
 echo " "
-echo -e "${TITLE}Elevating permissions before starting installation${ORANGE_NORMAL}"
+echof ${TITLE} "Elevating permissions before starting installation"
 sudo clear
 
-echo -e -n "${NORMAL}"
-
 sleep 0.5
-
 
 print_center ' _                  _      _                    __  _  _            '
 print_center '| |                | |    (_)                  / _|(_)| |           '
@@ -137,46 +144,70 @@ print_center '| | | || (_| |\__ \| | | || || | | || (_) | _ | |  | || ||  __/\__
 print_center '|_| |_| \__,_||___/|_| |_||_||_| |_| \___/ (_)|_|  |_||_| \___||___/'
 
 echo " "
-echo -e "${TITLE}Welcome to Hashino's dotfiles install script${NORMAL}"
+echof ${TITLE} "Welcome to Hashino's dotfiles install script"
 
 cd ${HOME}
 
-####################################################################################################
+##############################################################################
 # LOG FILE
-echo " "
-echo -e "${TITLE}Creating a new log file in: ${ORANGE}${log_file}${NORMAL}"
-#resets install log
-echo "" > $log_file
 
-####################################################################################################
-# CHOOSING IF USING PERSIST OR NOPASS
+echo " "
+echof ${TITLE} "Creating a new log file in: ${ORANGE}${log_file}"
+#resets install log
+echo "" >> $log_file
+
+##############################################################################
+# INSTALLING NEEDED PACKAGES FOR SCRIPT
+needed_packages="base-devel git opendoas"
+
+cd $HOME
+
+echo " "
+echof ${TITLE} "Installing packages needed for the script (${ORANGE_NORMAL}${needed_packages}${NORMAL})" 1
+sudo pacman -Syu ${needed_packages} --noconfirm --needed >> $log_file 2>&1 & spinner $!
+check_success
+
+##############################################################################
+# YAY
+
+echo " "
+echof $TITLE "Installing ${QUOTE}yay"
+
+echof $NORMAL "Cloning the ${ORANGE}yay${NORMAL} repository" 1
+git clone https://aur.archlinux.org/yay.git >> $log_file 2>&1 & spinner $!
+check_success
+
+echof $NORMAL "Building ${ORANGE}yay" 1
+cd yay
+xterm -e 'yes | makepkg -si | tee ${log_file}' >> $log_file 2>&1 & spinner $!
+check_success
+
+echof $NORMAL "Deleting install files" 1
+cd .. 
+rm -rf yay >> $log_file 2>&1 & spinner $!
+check_success
+
+##############################################################################
+# DOAS: CHOOSING IF USING PERSIST OR NOPASS
 
 echo -e "${BOLD}"
 read -r -p "Configure doas to use nopass instead of persist? [Y/n]" response
 response=${response,,} # tolower
 echo -e -n "${NORMAL}"
+
 if [[ $response =~ ^(y| ) ]] || [[ -z $response ]]; then
   doas_option="nopass"
-  echo -e "${ORANGE_NORMAL}nopass${NORMAL} option chosen. After sudo is replaced with doas, the rest of the installation will be done automatically"
+  echof ${ORANGE_NORMAL} "nopass${NORMAL} option chosen. After sudo is replaced with doas, the rest of the installation will be done automatically"
 else
-  echo -e "${RED}WARNING:${NORMAL} Because you chose the ${ORANGE_NORMAL}persist${NORMAL} option, you'll need to mannualy input your password multiple times during this script"
+  echof ${RED} "WARNING:${NORMAL} Because you chose the ${ORANGE_NORMAL}persist${NORMAL} option, you'll need to mannualy input your password multiple times during this script"
 fi
 
-####################################################################################################
-# INSTALLING NEEDED PACKAGES FOR SCRIPT
-cd $HOME
+# DOAS: INSTALLING DOAS AND REPLACING SUDO
 
 echo " "
-echo -e -n "${TITLE}Installing packages needed for the script ${NORMAL}(${ORANGE_NORMAL}git base-devel opendoas${NORMAL})"
-sudo pacman -Syu --needed git base-devel opendoas --noconfirm >> $log_file 2>&1 & spinner $!
-check_success
+echof ${TITLE} "Configuring ${ORANGE}doas${TITLE} and replacing ${BLUE}sudo"
 
-####################################################################################################
-# INSTALLING DOAS AND REPLACING SUDO
-echo " "
-echo -e "${TITLE}Configuring ${ORANGE}doas${TITLE} and replacing ${BLUE}sudo${NORMAL}"
-
-echo -e -n "Creating and moving the ${BLUE}doas.conf${NORMAL} file with the the chosen option (${doas_option})"
+echof $NORMAL "Creating and moving the ${BLUE}doas.conf${NORMAL} file with the the chosen option (${doas_option})"
 
 echo "permit setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel" > "${HOME}/doas.conf"
 echo "permit ${doas_option} :wheel" >> "${HOME}/doas.conf"
@@ -185,95 +216,79 @@ sudo cp "${HOME}/doas.conf" /etc/doas.conf
 rm "${HOME}/doas.conf"
 check_success
 
-echo -e -n "Cloning the AUR repository for ${ORANGE_NORMAL}doas-sudo-shim${NORMAL}"
-git clone https://aur.archlinux.org/doas-sudo-shim.git >> $log_file 2>&1 & spinner $!
+echof $NORMAL "Installing ${ORANGE_NORMAL}doas-sudo-shim${NORMAL}" 1
+yes | yay -Syu doas-sudo-shim --removemake --answerclean=n --answerdiff=n >> $log_file 2>&1 & spinner $!
 check_success
 
-echo -e -n "Building package"
-cd doas-sudo-shim
-yes | makepkg -si  >> $log_file 2>&1 & spinner $!
-check_success
+##############################################################################
 
-echo -e -n "Deleting install files"
-cd .. 
-doas rm -R doas-sudo-shim >> $log_file 2>&1 & spinner $!
-check_success
-####################################################################################################
-# YAY
 echo " "
-echo -e "${TITLE}Installing ${QUOTE}yay${NORMAL}"
-
-echo -e -n "Cloning the ${ORANGE}yay${NORMAL} repository "
-git clone https://aur.archlinux.org/yay.git >> $log_file 2>&1 & spinner $!
-check_success
-
-echo -e -n "Building ${ORANGE}yay${NORMAL}"
-cd yay
-yes | makepkg -si >> $log_file 2>&1 & spinner $!
-check_success
-
-echo -e -n "Deleting install files"
-cd .. 
-doas rm -R yay >> $log_file 2>&1 & spinner $!
-check_success
-
-####################################################################################################
-echo " "
-echo -e -n "${TITLE}Initial setup done. Starting main installation${NORMAL}"
+echof ${TITLE} "Initial setup done. Starting main installation" 1
 sleep 2 & spinner $!
 cd $HOME
 echo " "
-####################################################################################################
+
+##############################################################################
 # CLONING DOTFILES
+
 echo " "
-echo -e -n "${TITLE}Cloning ${BLUE}${dotfiles_remote}${TITLE} to ${BLUE}${dotfiles_local}${NORMAL}"
+echof ${TITLE} "Cloning ${BLUE}${dotfiles_remote}${TITLE} to ${BLUE}${dotfiles_local}" 1
 git clone --recurse-submodules $dotfiles_remote $dotfiles_local >> $log_file 2>&1 & spinner $!
 check_success
 
-####################################################################################################
+##############################################################################
 # SYMLINKS
+
 echo " "
-echo -e "${TITLE}Creating symlinks between folders in ${ORANGE}.dotfiles/.config/${NORMAL}${TITLE} and ${BLUE}~/.config/${NORMAL}"
+echof ${TITLE} "Creating symlinks between folders in ${ORANGE}.dotfiles/.config/${NORMAL}${TITLE} and ${BLUE}~/.config" 1
 echo " "
 
 mkdir -p "${HOME}/.config/"
 
 cd "${dotfiles_local}/.config"
 #creates symlinks to all folder in dotfiles/.configs/ in ~/.config
-for app in */ ; do
-  remote_config="${dotfiles_local}/.config/${app}"
+for app_config in */ ; do
+  remote_config="${dotfiles_local}/.config/${app_config}"
   local_config="${HOME}/.config/"
-  
-  echo -e -n "Symlinking ${ORANGE}" 
-  echo -e -n $(head -c-2 <<< $app) 
-  echo -e -n "${NORMAL} -> ${BLUE}${remote_config}${NORMAL}"
-  
+
+  echof $NORMAL "Symlinking ${ORANGE}${remote_config}${NORMAL} -> ${BLUE}${local_config}${app_config}" 1
+
+  app_name=${app_config::-1}
+
+  #remove command
+  mv "${local_config}${app_name}" "${local_config}${app_name}_old" >> $log_file 2>&1
+  if [ $? -eq 0 ]; then
+    echof $ORANGE_NORMAL "old config found for: ${app_config} moved to ${app_config}_old"
+  fi
+
   #symlink command
   ln -s $remote_config $local_config >> $log_file 2>&1
-  
+
   check_success  
 done
 
-####################################################################################################
+##############################################################################
 # COMPOSER
+
 echo " "
-echo -e -n "Importing composer settings"
+echof $NORMAL "Importing composer settings" 1
 echo " "
 
 ln -s "${dotfiles_local}/.config/picom.conf" "${HOME}/.config/picom.conf" >> $log_file 2>&1
 
-####################################################################################################
+##############################################################################
 # REPLACING AWESOME WITH GIT VERSION
+
 echo " "
-echo -e -n "${TITLE}Removing ${ORANGE}awesome${NORMAL}..."
+echof $TITLE "Removing ${ORANGE}awesome${NORMAL}..." 1
 yay -Rcns --noconfirm awesome >> $log_file 2>&1 & spinner $!
 check_success
 
-echo -e -n "${TITLE}and installing ${ORANGE}awesome-git${NORMAL}..."
-yay -Syu awesome-git --noconfirm --askyesremovemake --needed >> $log_file 2>&1 & spinner $!
+echof $TITLE "and installing ${ORANGE}awesome-git${NORMAL}..." 1
+yay -Syu awesome-git --noconfirm --removemake --needed >> $log_file 2>&1 & spinner $!
 check_success
 
-####################################################################################################
+##############################################################################
 # PACKAGES TO INSTALL
 
 cp "${dotfiles_local}/.scripts/pkg.list" "${HOME}/pkg.list"
@@ -284,10 +299,11 @@ fi
 
 nano "${HOME}/pkg.list"
 
-####################################################################################################
+##############################################################################
 # INSTALLING PACKAGES
+
 echo " "
-echo -e "${TITLE}Installing packages in ${BLUE}~/pkg.list${NORMAL}${TITLE} using ${BLUE}yay${NORMAL}"
+echof ${TITLE} "Installing packages in ${BLUE}~/pkg.list${NORMAL}${TITLE} using ${BLUE}yay"
 echo " "
 
 #install all packages listed in /.dotfiles/.scripts/pkg.list
@@ -295,10 +311,10 @@ while read app; do
   
   #doesnt process lines that start with '#' to allow comments in the pkg.list file
   if [[ ! "$app" == \#* ]]; then
-    echo -e -n "Installing ${BLUE}${app}${NORMAL}"
+    echof $NORMAL "Installing ${BLUE}${app}${NORMAL}" 1
 
     #install command
-    yes | yay -S $app --noconfirm --askyesremovemake --needed >> $log_file 2>&1 & spinner $!
+    yay -S $app --noconfirm --removemake --needed >> $log_file 2>&1 & spinner $!
     check_success
   fi
 
@@ -306,60 +322,46 @@ done <"${HOME}/pkg.list"
 
 rm "${HOME}/pkg.list"
 
-####################################################################################################
+##############################################################################
 # THEMING
-echo " "
-echo -e -n "${TITLE}Importing ${QUOTE}wallpaper${NORMAL}"
-ln -s "${dotfiles_local}/wallpaper.png" "${HOME}" >> $log_file 2>&1
-check_success
 
 echo " "
-echo -e -n "${TITLE}Installing cursor theme ${QUOTE}Simp1e-Nord-Darker-Hash${NORMAL}"
-ln -s "${dotfiles_local}/.icons/" "${HOME}/" >> $log_file 2>&1
-check_success
-
-echo " "
-echo -e -n "${TITLE}Applying GTK configs${NORMAL}"
+echof $TITLE "Symlinking GTK 2.0 configs" 1
 ln -s "${dotfiles_local}/.gtkrc-2.0" "${HOME}/" >> $log_file 2>&1
 check_success
 
 #folder theme: papirus nord polar night 3 https://github.com/Adapta-Projects/Papirus-Nord
 echo " "
-echo -e "${TITLE}Applying folder theme ${QUOTE}Papirus Nord Polar Night 3${NORMAL}"
+echof $TITLE "Applying folder theme ${QUOTE}Papirus Nord Polar Night 3"
 echo " "
 
-echo -e -n "Installing ${BLUE}papirus-folders${NORMAL}"
+echof $NORMAL "Installing ${BLUE}papirus-folders${NORMAL}" 1
 cd "${HOME}/.config/Papirus-Nord/"
 echo "Y" | doas ./install >> $log_file 2>&1 & spinner $!
 check_success
 
-echo -e -n "Applying theme"
+echof $NORMAL "Applying folder theme" 1
 doas ./papirus-folders -C polarnight3 >> $log_file 2>&1 & spinner $!
-check_success
-
-#local binaries: rofi-power-menu rofi-todo soundswitch
-echo " "
-echo -e -n "${TITLE}Symlinking ${QUOTE}.local/${NORMAL}${TITLE} to home${NORMAL}"
-ln -s "${dotfiles_local}/.local/bin" "${HOME}/.local/" >> $log_file 2>&1
 check_success
 
 #setting fish as default shell
 echo " "
-echo -e -n "${TITLE}Changing user shell to ${QUOTE}fish${NORMAL}"
+echof $TITLE "Changing user shell to ${QUOTE}fish${NORMAL}" 1
 doas chsh --shell /bin/fish $user >> $log_file 2>&1
 check_success
 
-####################################################################################################
+##############################################################################
 # CLEANUP
-echo -e -n "${TITLE}Uninstalling ${ORANGE}alacritty${NORMAL}"
+
+echof $TITLE "Uninstalling ${ORANGE}alacritty" 1
 yes | yay -Rns alacritty >> $log_file 2>&1 & spinner $!
 check_success
 
 echo " "
-echo -e "${TITLE}Installation Complete${NORMAL}"
+echof $TITLE "Installation Complete"
 
 echo " "
-echo "Deleting install files and restarting Xorg in "
+echof $NORMAL "Deleting install files and restarting Xorg in "
 
 echo -n '5...'
 sleep 1
@@ -380,8 +382,6 @@ rm -f .bashrc
 
 rm -f .wget-hst
 
-doas rm -R .config/yay 
-
-doas rm -R go
+rm -rf go
 
 rm -f install_dotfiles.sh
