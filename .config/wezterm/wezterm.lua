92a9c08d46cbe3d138b8126734f175496b4bbc0c
Hashino <iam.ravara@gmail.com>
2024-12-28 17:06:33 -0300

    tweaks for wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.colors = require('hash.colors')
+
+config.font = wezterm.font("UbuntuMono Nerd Font",
+  { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font_size = 18.0
+
+config.keys = {
+  {
+    key = 'C',
+    mods = 'CTRL',
+    action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
+  },
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_close_confirmation = "NeverPrompt"
+
+return config
